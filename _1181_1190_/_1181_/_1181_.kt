bf7fab03fa0f610da945e21e6ce2a9ceda013f58
Marvin <marvinwanzuita@gmail.com>
2023-01-18 14:42:42 -0300

    Exercicio 1181

---
+++ _1181_1190_/_1181_/_1181_.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+    val rows = 12
+    val cols = 12
+
+    val L = readLine()!!.toInt()
+    val T = readLine()
+    var total: Double = 0.0
+
+    var arr = Array(rows) { r -> DoubleArray(cols) { readLine()!!.toDouble() } }
+
+    for (i in 0 until rows){
+
+        if (i == L){
+
+            for (j in 0 until cols){
+
+                total += arr[i][j]
+
+            }
+
+        }
+    }
+
+    if (T == "S"){
+        println("%.1f".format(total))
+    } else {
+        println("%.1f".format(total / cols))
+    }
+
+}
+
