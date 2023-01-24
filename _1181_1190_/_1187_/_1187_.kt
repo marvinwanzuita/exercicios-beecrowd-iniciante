d4aff5ce83704e50ebbd004e38c323525c335617
Marvin <marvinwanzuita@gmail.com>
2023-01-24 10:09:03 -0300

    Exercicio 1187

---
+++ _1181_1190_/_1187_/_1187_.kt
@@ -0,0 +1,34 @@
+fun main(){
+
+    val rows = 12
+    val cols = 12
+
+    val T = readLine()
+    var quantidadeSoma = 0
+    var total: Double = 0.0
+    var indicarColuna: Int
+    var indicarLinha: Int
+
+    val arr = Array(rows) { r -> DoubleArray(cols) { readLine()!!.toDouble() } }
+
+    indicarColuna = arr.size - 1
+    indicarLinha = 0
+
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            if (j > indicarLinha && j < indicarColuna) {
+                total += arr[i][j]
+                quantidadeSoma++
+            }
+        }
+        indicarLinha++
+        indicarColuna--
+    }
+
+    if (T == "S") {
+        println("%.1f".format(total))
+    } else if (T == "M") {
+        println("%.1f".format(total / quantidadeSoma))
+    }
+
+}
