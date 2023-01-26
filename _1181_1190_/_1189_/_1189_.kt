42afdd40b8af942d4d2af68a749cebab42eeef5e
Marvin <marvinwanzuita@gmail.com>
2023-01-26 14:02:57 -0300

    Exercicio 1189

---
+++ _1181_1190_/_1189_/_1189_.kt
@@ -0,0 +1,40 @@
+
+fun main(){
+
+    val rows = 12
+    val cols = 12
+
+    val T = readLine()
+    var quantidadeSoma = 0
+    var total: Double = 0.0
+
+    val arr = Array(rows) { r -> DoubleArray(cols) { readLine()!!.toDouble() } }
+
+    var metadeColuna: Int  = (arr.size / 2) - 1
+    var posicaoMatrix = 0
+
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+
+            if (j < posicaoMatrix){
+                total += arr[i][j]
+                quantidadeSoma++
+            }
+
+        }
+
+        if (i < metadeColuna ) {
+            posicaoMatrix++
+        } else if (i > metadeColuna) {
+            posicaoMatrix--
+        }
+
+    }
+
+    if (T == "S") {
+        println("%.1f".format(total))
+    } else if (T == "M") {
+        println("%.1f".format(total / quantidadeSoma))
+    }
+
+}
