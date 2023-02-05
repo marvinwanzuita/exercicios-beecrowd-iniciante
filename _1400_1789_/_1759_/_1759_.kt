2c921813ac16ab96892445dd44843a80ec0b3a46
Marvin <marvinwanzuita@gmail.com>
2023-02-05 11:16:37 -0300

    Exercicio 1759

---
+++ _1400_1789_/_1759_/_1759_.kt
@@ -0,0 +1,19 @@
+package beecrowd.iniciante._1400_1789_._1759_
+
+fun main(){
+
+    val N = readLine()?.toInt()!!
+
+    for (i in 0 until N){
+
+        if (i == N - 1){
+            print("Ho!")
+            println()
+        } else {
+            print("Ho ")
+        }
+    }
+
+
+
+}
