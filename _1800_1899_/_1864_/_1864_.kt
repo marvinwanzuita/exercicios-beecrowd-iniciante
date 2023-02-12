362abd29c1929eb36a9c22b83da9d1308f5523fc
Marvin <marvinwanzuita@gmail.com>
2023-02-12 10:18:43 -0300

    Exercicio 1864

---
+++ _1800_1899_/_1864_/_1864_.kt
@@ -0,0 +1,16 @@
+package beecrowd.iniciante._1800_1899_._1864_
+
+fun main(){
+
+    val N = readLine()?.toInt()
+    val citacao = "LIFE IS NOT A PROBLEM TO BE SOLVED"
+
+
+    for (i in 0 until N!!){
+
+        print(citacao[i].toChar())
+
+    }
+    println()
+
+}
