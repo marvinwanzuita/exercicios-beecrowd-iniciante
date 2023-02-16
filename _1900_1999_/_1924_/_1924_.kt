ca51e0cef9ce65dfc44ef4f8a0991ad398fb8a72
Marvin <marvinwanzuita@gmail.com>
2023-02-16 09:45:28 -0300

    Exercicio 1924

---
+++ _1900_1999_/_1924_/_1924_.kt
@@ -0,0 +1,18 @@
+package beecrowd.iniciante._1900_1999_._1924_
+
+fun main(){
+
+    val N = readLine()?.toInt()
+    var S: String
+
+    for (i in 0 until N!!) {
+
+        S = readLine().toString()
+
+    }
+
+    println("Ciencia da Computacao")
+
+
+
+}
