3ff891aba06992a737b243715664a2031504aabb
Marvin <marvinwanzuita@gmail.com>
2023-03-25 14:44:33 -0300

    Exercicio 2165

---
+++ _2100_2199_/_2165_/_2165_.kt
@@ -0,0 +1,13 @@
+package beecrowd.iniciante._2100_2199_._2165_
+
+fun main(){
+
+    var tweet = readLine().toString()
+
+    var tamanhoTweet = tweet.length
+
+    if (tamanhoTweet <= 140) println("TWEET") else println("MUTE")
+
+
+
+}
