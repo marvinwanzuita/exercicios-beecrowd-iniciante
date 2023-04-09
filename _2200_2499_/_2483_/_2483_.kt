2e0c1c99c0eab51611b11a126b255b9e826d331e
Marvin <marvinwanzuita@gmail.com>
2023-04-09 15:12:53 -0300

    Exercicio 2483

---
+++ _2200_2499_/_2483_/_2483_.kt
@@ -0,0 +1,17 @@
+package beecrowd.iniciante._2200_2499_._2483_
+
+fun main(){
+
+    var indiceFelicidade = readLine()!!.toInt()
+
+    var quantidadeA = ""
+
+    for (i in 0 until indiceFelicidade){
+        quantidadeA += "a"
+    }
+
+    println("Feliz nat${quantidadeA}l!")
+
+
+
+}
