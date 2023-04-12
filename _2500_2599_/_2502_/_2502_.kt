1b8fbd6dae8fec47d65a7a8a23dd4cafa6016b14
Marvin <marvinwanzuita@gmail.com>
2023-04-12 14:42:08 -0300

    Exercicio 2502 - Tentado

---
+++ _2500_2599_/_2502_/_2502_.kt
@@ -0,0 +1,38 @@
+package beecrowd.iniciante._2500_2599_._2502_
+
+import java.util.*
+
+fun main(){
+
+    val sc = Scanner(System.`in`)
+
+    var cifraLinhas : String
+    var cifraLinhasArray : Array<String>
+
+    var tamanhoCifra : Int
+    var quantidadeLinhas : Int
+
+    var cifraEntrada : String
+    var cifraSaida : String
+
+    var listaFrasesParaCriptografar = mutableListOf<String>()
+    var listaFrasesCriptografadas = mutableListOf<String>()
+
+    while (sc.hasNext()){
+
+        cifraLinhas = sc.nextLine()
+        cifraLinhasArray = cifraLinhas.split(" ").toTypedArray()
+
+        tamanhoCifra = cifraLinhasArray[0].toInt()
+        quantidadeLinhas = cifraLinhasArray[1].toInt()
+
+
+
+
+
+
+    }
+
+
+
+}
