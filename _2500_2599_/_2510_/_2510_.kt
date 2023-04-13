c3726f8af274111a8011bba0b8b3c57b6fc4c2f3
Marvin <marvinwanzuita@gmail.com>
2023-04-13 08:55:33 -0300

    Exercicio 2510

---
+++ _2500_2599_/_2510_/_2510_.kt
@@ -0,0 +1,15 @@
+package beecrowd.iniciante._2500_2599_._2510_
+
+fun main(){
+
+    var entradas = readLine()!!.toInt()
+    var vilao : String
+
+    for (i in 0 until entradas){
+        vilao = readLine().toString()
+        println("Y")
+    }
+
+
+
+}
