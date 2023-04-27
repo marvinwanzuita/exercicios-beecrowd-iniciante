3414f059fafd08f71c97664fc5f95af2f2283c06
Marvin <marvinwanzuita@gmail.com>
2023-04-27 09:06:52 -0300

    Exercicio 2581

---
+++ _2500_2599_/_2581_/_2581_.kt
@@ -0,0 +1,14 @@
+package beecrowd.iniciante._2500_2599_._2581_
+
+fun main(){
+
+    var quantidadePerguntas = readLine()!!.toInt()
+    var perguntas : String
+
+    for (i in 0 until quantidadePerguntas){
+        perguntas = readLine().toString()
+        println("I am Toorg!")
+    }
+
+
+}
