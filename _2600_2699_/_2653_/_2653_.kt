ab71feb676253c1ca872eaf574dea4c0c0a28f7a
Marvin <marvinwanzuita@gmail.com>
2023-05-06 10:56:52 -0300

    Exercicio 2653

---
+++ _2600_2699_/_2653_/_2653_.kt
@@ -0,0 +1,18 @@
+package beecrowd.iniciante._2600_2699_._2653_
+
+import java.util.*
+
+fun main(){
+
+    val sc = Scanner(System.`in`)
+    var listaJoias = mutableSetOf<String>()
+
+    while (sc.hasNext()) {
+        listaJoias.add(sc.nextLine())
+    }
+
+    println(listaJoias.size)
+
+
+
+}
