b5df63b9f380990fbb0683c05471ddaa4271c57c
Marvin <marvinwanzuita@gmail.com>
2023-05-23 10:27:26 -0300

    Exercicio 2747

--- _2700_2759_/_2747_/_2747_.kt
+++ _2700_2759_/_2747_/_2747_.kt
@@ -2,14 +2,26 @@ package beecrowd.iniciante._2700_2759_._2747_
 
 fun main(){
 
-    for(i in 0 until 7){
+    for(i in 1 until 8){
 
-        for (j in 0 until 39){
+        for (j in 1 until 40){
 
-            if (i == 0 || i == 6) {
+            if (i == 1 || i == 7) {
                 print("-")
-            } else if (j == 0 || j == 38) {
+            } else if (j == 1 || j == 39) {
                 print("|")
+            } else if (i == 2 && j == 10) {
+                print("Roberto")
+            }  else if (i == 2 && (j in 11..16)) {
+                continue
+            } else if (i == 4 && j == 10) {
+                print("5786")
+            }  else if (i == 4 && (j in 11..13)) {
+                continue
+            } else if (i == 6 && j == 10) {
+                print("UNIFEI")
+            }  else if (i == 6 && (j in 11..15)) {
+                continue
             } else {
                 print(" ")
             }
