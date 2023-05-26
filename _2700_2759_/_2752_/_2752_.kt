443aed41e94be7c1583465196f017f0686c9b1f4
Marvin <marvinwanzuita@gmail.com>
2023-05-26 10:54:52 -0300

    Exercicio 2752

---
+++ _2700_2759_/_2752_/_2752_.kt
@@ -0,0 +1,26 @@
+package beecrowd.iniciante._2700_2759_._2752_
+
+fun main(){
+
+    var frase = "AMO FAZER EXERCICIO NO URI"
+
+    System.out.printf("<" + "%s" + ">", frase)
+    println()
+    System.out.printf("<" + "%30s" + ">", frase)
+    println()
+    System.out.printf("<" + "%.20s" + ">", frase)
+    println()
+    System.out.printf("<" + "%-20s" + ">", frase)
+    println()
+    System.out.printf("<" + "%-30s" + ">", frase)
+    println()
+    System.out.printf("<" + "%.30s" + ">", frase)
+    println()
+    System.out.printf("<" + "%30.20s" + ">", frase)
+    println()
+    System.out.printf("<" + "%-30.20s" + ">", frase)
+    println()
+
+
+
+}
