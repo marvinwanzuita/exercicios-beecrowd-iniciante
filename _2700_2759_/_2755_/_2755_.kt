3404fddc28a1f5bf2c8d4276e5268f0a7c4551d4
Marvin <marvinwanzuita@gmail.com>
2023-05-29 09:54:45 -0300

    Exercicio 2755

---
+++ _2700_2759_/_2755_/_2755_.kt
@@ -0,0 +1,10 @@
+package beecrowd.iniciante._2700_2759_._2755_
+
+fun main(){
+
+    println("\"Ro'b'er\tto\\/\"")
+    println("(._.) ( l: ) ( .-. ) ( :l ) (._.)")
+    println("(^_-) (-_-) (-_^)")
+    println("(\"_\") ('.')")
+
+}
