851545e3598d93b7fe9d9589935a9514bf58e84c
Marvin <marvinwanzuita@gmail.com>
2023-05-30 09:50:33 -0300

    Exercicio 2756

---
+++ _2700_2759_/_2756_/_2756_.kt
@@ -0,0 +1,16 @@
+package beecrowd.iniciante._2700_2759_._2756_
+
+fun main(){
+
+    println("       A")
+    println("      B B")
+    println("     C   C")
+    println("    D     D")
+    println("   E       E")
+    println("    D     D")
+    println("     C   C")
+    println("      B B")
+    println("       A")
+
+
+}
