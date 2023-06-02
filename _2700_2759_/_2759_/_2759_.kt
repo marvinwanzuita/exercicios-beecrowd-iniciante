14db2497966d7b6e3e94a6f422d25b0361909e68
Marvin <marvinwanzuita@gmail.com>
2023-06-02 09:32:35 -0300

    Exercicio 2759

---
+++ _2700_2759_/_2759_/_2759_.kt
@@ -0,0 +1,13 @@
+package beecrowd.iniciante._2700_2759_._2759_
+
+fun main(){
+
+    var l1 = readLine().toString()
+    var l2 = readLine().toString()
+    var l3 = readLine().toString()
+
+    println("A = $l1, B = $l2, C = $l3")
+    println("A = $l2, B = $l3, C = $l1")
+    println("A = $l3, B = $l1, C = $l2")
+
+}
