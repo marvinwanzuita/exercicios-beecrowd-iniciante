3616648ee3d2c5a40ca0089400f810fa8a420178
Marvin <marvinwanzuita@gmail.com>
2023-06-05 10:10:06 -0300

    Exercicio 2762

---
+++ _2760_2799_/_2762_/_2762_.kt
@@ -0,0 +1,14 @@
+package beecrowd.iniciante._2760_2799_._2762_
+
+fun main(){
+
+    var decimal = readLine().toString()
+
+    var (n1, n2) = decimal.split(".")
+
+    var n2Int = n2.toInt()
+
+    println("$n2Int.$n1")
+
+
+}
