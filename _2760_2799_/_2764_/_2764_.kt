52c9267655d87bbd60526289b15c0c2cddcdf3bc
Marvin <marvinwanzuita@gmail.com>
2023-06-07 13:42:07 -0300

    Exercicio 2764

---
+++ _2760_2799_/_2764_/_2764_.kt
@@ -0,0 +1,12 @@
+package beecrowd.iniciante._2760_2799_._2764_
+
+fun main(){
+
+    var (dia, mes, ano) = readLine()!!.split("/")
+
+    println("$mes/$dia/$ano")
+    println("$ano/$mes/$dia")
+    println("$dia-$mes-$ano")
+
+
+}
