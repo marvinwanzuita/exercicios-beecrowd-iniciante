fa5dfa192abc19e3586185027596fcff35063ca2
Marvin <marvinwanzuita@gmail.com>
2023-06-09 09:13:50 -0300

    Exercicio 2765

---
+++ _2760_2799_/_2765_/_2765_.kt
@@ -0,0 +1,11 @@
+package beecrowd.iniciante._2760_2799_._2765_
+
+fun main(){
+
+    var (inicioFrase, finalFrase) = readLine()!!.split(",")
+
+    println(inicioFrase)
+    println(finalFrase)
+
+
+}
