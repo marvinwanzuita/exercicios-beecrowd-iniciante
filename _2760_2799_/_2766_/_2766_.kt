260714c75dda47be7fc5396e00a5716ff188b414
Marvin <marvinwanzuita@gmail.com>
2023-06-10 11:54:38 -0300

    Exercicio 2766

---
+++ _2760_2799_/_2766_/_2766_.kt
@@ -0,0 +1,16 @@
+package beecrowd.iniciante._2760_2799_._2766_
+
+fun main(){
+
+    var nomes = mutableListOf<String>()
+
+    for (i in 0..9){
+        nomes.add(readLine().toString())
+    }
+
+    println(nomes[2])
+    println(nomes[6])
+    println(nomes[8])
+
+
+}
